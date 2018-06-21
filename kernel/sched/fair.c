4737f61e20093eb19656716d0638cd4589418995
Wei Wang <wvw@google.com>
2018-06-20 22:50:53 -0700

    sched: fair: skip energy aware placement when overutilized
    Only place waking tasks on the most energy efficient cpu, when the
    system is not overutilized.
    
    Bug: 110457179
    Bug: 110700346
    Change-Id: I29f5d384dc93ff137c90f63cfea65b4ee91897fe
    Signed-off-by: Wei Wang <wvw@google.com>
    [Minof fixup]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7738,7 +7738,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 			cpumask_test_cpu(cpu, tsk_cpus_allowed(p)));
 	}
 
-	if (energy_aware()) {
+	if (energy_aware() && !(cpu_rq(prev_cpu)->rd->overutilized)) {
 		rcu_read_lock();
 		/*
 		 * If the sync flag is set but ignored, prefer to
