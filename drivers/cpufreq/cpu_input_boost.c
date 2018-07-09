5a4a69a53330acd79641b692c2bb6735cde59a3c
joshuous <joshuous@gmail.com>
2018-07-09 17:24:37 +0200

    sched/tune: Switch Dynamic Schedtune Boost to a slot-based tracking system
    Switch from a counter-based system to a slot-based system for managing
    multiple dynamic Schedtune boost requests.
    
    The primary limitations of the counter-based system was that it could
    only keep track of two boost values at a time: the current dynamic boost
    value and the default boost value. When more than one boost request is
    issued, the system would only remember the highest value of them all.
    Even if the task that requested the highest value had unboosted, this
    value is still maintained as long as there are other active boosts that
    are still running. A more ideal outcome would be for the system to
    unboost to the maximum boost value of the remaining active boosts.
    
    The slot-based system provides a solution to the problem by keeping
    track of the boost values of all ongoing active boosts. It ensures that
    the current boost value will be equal to the maximum boost value of
    all ongoing active boosts. This is achieved with two linked lists
    (active_boost_slots and available_boost_slots), which assign and keep
    track of boost slot numbers for each successful boost request. The boost
    value of each request is stored in an array (slot_boost[]), at an index
    value equal to the assigned boost slot number.
    
    For now we limit the number of active boost slots to 5 per Schedtune
    group.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -24,6 +24,13 @@ static unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
 static unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static bool stune_boost_active;
+static int boost_slot;
+static unsigned short dynamic_stune_boost;
+module_param(dynamic_stune_boost, short, 0644);
+#endif
+
 module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
 module_param(input_boost_duration, short, 0644);
@@ -150,6 +157,10 @@ static void input_boost_worker(struct work_struct *work)
 		update_online_cpu_policy();
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	if (!do_stune_boost("top-app", dynamic_stune_boost, &boost_slot))
+		stune_boost_active = true;
+#endif
 	queue_delayed_work(b->wq, &b->input_unboost,
 		msecs_to_jiffies(input_boost_duration));
 }
@@ -160,6 +171,13 @@ static void input_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), input_unboost);
 
 	clear_boost_bit(b, INPUT_BOOST);
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	if (stune_boost_active) {
+		reset_stune_boost("top-app", boost_slot);
+		stune_boost_active = false;
+	}
+#endif
 	update_online_cpu_policy();
 }
 
@@ -172,6 +190,10 @@ static void max_boost_worker(struct work_struct *work)
 		update_online_cpu_policy();
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	if (!do_stune_boost("top-app", dynamic_stune_boost, &boost_slot))
+		stune_boost_active = true;
+#endif
 	queue_delayed_work(b->wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
 }
@@ -182,6 +204,13 @@ static void max_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), max_unboost);
 
 	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	if (stune_boost_active) {
+		reset_stune_boost("top-app", boost_slot);
+		stune_boost_active = false;
+	}
+#endif
 	update_online_cpu_policy();
 }
 
@@ -288,6 +317,12 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	if (stune_boost_active) {
+		reset_stune_boost("top-app", boost_slot);
+		stune_boost_active = false;
+	}
+#endif
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
