0da04cdc35af05646a382e7dda7d8a0177474067
AlekDevOfficial <alekdev001@gmail.com>
2018-11-26 16:32:38 +0100

    GPU OC to 833 mhz
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -279,9 +279,9 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(342000000, P_CRC_DIV,  1, 0, 0),
 	F(414000000, P_CRC_DIV,  1, 0, 0),
 	F(520000000, P_CRC_DIV,  1, 0, 0),
-	F(596000000, P_CRC_DIV,  1, 0, 0),
-	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
+	F(767000000, P_CRC_DIV,  1, 0, 0),
+	F(833000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -636,10 +636,10 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
 		520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		596000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		710000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 767000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
+		833000000;	;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
