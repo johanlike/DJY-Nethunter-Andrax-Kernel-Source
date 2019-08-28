78c41fd91faa88c2bda6dce2f6265c8586a08e36
Danny Lin <danny@kdrag0n.dev>
2019-08-27 23:54:00 -0700

    PM / sleep: Skip OOM killer toggles when a LMK is in use
    CONFIG_HAVE_LOW_MEMORY_KILLER disables the OOM killer, so there's no
    reason to disable and enable the OOM killer when entering and exiting
    suspend.
    
    This is a fixed version of https://github.com/YaroST12/VIOLENT_kernel/commit/86e59a93b2ef
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -152,6 +152,7 @@ int freeze_processes(void)
 	pr_cont("\n");
 	BUG_ON(in_atomic());
 
+#ifndef CONFIG_HAVE_LOW_MEMORY_KILLER
 	/*
 	 * Now that the whole userspace is frozen we need to disbale
 	 * the OOM killer to disallow any further interference with
@@ -160,6 +161,7 @@ int freeze_processes(void)
 	 */
 	if (!error && !oom_killer_disable(msecs_to_jiffies(freeze_timeout_msecs)))
 		error = -EBUSY;
+#endif
 
 	if (error)
 		thaw_processes();
@@ -204,7 +206,9 @@ void thaw_processes(void)
 	pm_freezing = false;
 	pm_nosig_freezing = false;
 
+#ifndef CONFIG_HAVE_LOW_MEMORY_KILLER
 	oom_killer_enable();
+#endif
 
 	pr_info("Restarting tasks ... ");
 
