38489b5f4003e247c3eaff96d8f7f1dcbe56eff9
Gopi Sai Teja <gopi.teja@oneplus.com>
2019-10-24 12:54:55 +0530

    Add debug change for modem subsystem
    Change-Id: Id43b2704d43b9a38fc4b133259ff2ead2c37f933
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -43,7 +43,7 @@
 
 #define subsys_to_drv(d) container_of(d, struct modem_data, subsys_desc)
 
-static void log_modem_sfr(void)
+static void log_modem_sfr(struct modem_data *drv)
 {
 	u32 size;
 	char *smem_reason, reason[MAX_SSR_REASON_LEN], *function_name;
@@ -61,14 +61,15 @@ static void log_modem_sfr(void)
 
 	strlcpy(reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
 	function_name = parse_function_builtin_return_address((unsigned long)__builtin_return_address(0));
-        save_modem_dump_reason_to_device_info(reason);
+	save_modem_dump_reason_to_device_info(reason);
 	save_dump_reason_to_smem(reason, function_name);
 	pr_err("modem subsystem failure reason: %s.\n", reason);
+	subsys_store_crash_reason(drv->subsys, reason);
 }
 
 static void restart_modem(struct modem_data *drv)
 {
-	log_modem_sfr();
+	log_modem_sfr(drv);
 	drv->ignore_errors = true;
 	subsystem_restart_dev(drv->subsys);
 }
