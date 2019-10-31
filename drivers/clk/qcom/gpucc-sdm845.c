7d2f3a00490273d87e8ba446cf4caea947a3a84e
Pierre2324 <pdbbasketball@gmail.com>
2019-10-31 00:19:34 -0400

    sdm845: max gpu freq -> 835MHz
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -281,7 +281,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(520000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
 	F(767000000, P_CRC_DIV,  1, 0, 0),
-	F(833000000, P_CRC_DIV,  1, 0, 0),
+	F(835000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -639,7 +639,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 767000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		833000000;	;
+		835000000;	;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
