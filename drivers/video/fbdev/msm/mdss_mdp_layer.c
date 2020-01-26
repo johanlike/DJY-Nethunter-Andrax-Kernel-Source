968f8405d10367df997aa5fc036e6b55fb119bee
Sultan Alsawaf <sultan@kerneltoast.com>
2020-01-26 13:06:55 +0100

    msm: mdss: Remove sync debug name generation from hot path
    Generating a sync debug name with snprintf in a hot path results in
    excessive CPU time wasted on unneeded debug info. Remove the name
    generation entirely to cut down CPU waste in the frame render hot path.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Johanlike <17888022+johanlike@users.noreply.github.com>

--- drivers/video/fbdev/msm/mdss_mdp_layer.c
+++ drivers/video/fbdev/msm/mdss_mdp_layer.c
@@ -716,7 +716,6 @@ static struct mdss_fence *__create_fence(struct msm_fb_data_type *mfd,
 	struct mdss_overlay_private *mdp5_data;
 	struct mdss_mdp_ctl *ctl;
 	struct mdss_fence *sync_fence = NULL;
-	char fence_name[32];
 
 	mdp5_data = mfd_to_mdp5_data(mfd);
 
@@ -731,13 +730,6 @@ static struct mdss_fence *__create_fence(struct msm_fb_data_type *mfd,
 		return ERR_PTR(-EPERM);
 	}
 
-	if (fence_type == MDSS_MDP_RETIRE_FENCE)
-		snprintf(fence_name, sizeof(fence_name), "fb%d_retire",
-			mfd->index);
-	else
-		snprintf(fence_name, sizeof(fence_name), "fb%d_release",
-			mfd->index);
-
 	if ((fence_type == MDSS_MDP_RETIRE_FENCE) &&
 		(mfd->panel.type == MIPI_CMD_PANEL)) {
 		if (sync_pt_data->timeline_retire) {
@@ -745,7 +737,7 @@ static struct mdss_fence *__create_fence(struct msm_fb_data_type *mfd,
 				mdp5_data->retire_cnt++;
 			sync_fence = mdss_fb_sync_get_fence(
 				sync_pt_data->timeline_retire,
-				fence_name, value);
+				"", value);
 		} else {
 			return ERR_PTR(-EPERM);
 		}
@@ -753,16 +745,16 @@ static struct mdss_fence *__create_fence(struct msm_fb_data_type *mfd,
 		if (fence_type == MDSS_MDP_RETIRE_FENCE)
 			sync_fence = mdss_fb_sync_get_fence(
 						sync_pt_data->timeline_retire,
-						fence_name, value);
+						"", value);
 		else
 			sync_fence = mdss_fb_sync_get_fence(
 						sync_pt_data->timeline,
-						fence_name, value);
+						"", value);
 
 	}
 
 	if (IS_ERR_OR_NULL(sync_fence)) {
-		pr_err("%s: unable to retrieve release fence\n", fence_name);
+		pr_err("%s: unable to retrieve release fence\n", __func__);
 		goto end;
 	}
 
@@ -770,7 +762,7 @@ static struct mdss_fence *__create_fence(struct msm_fb_data_type *mfd,
 	*fence_fd = mdss_get_sync_fence_fd(sync_fence);
 	if (*fence_fd < 0) {
 		pr_err("%s: get_unused_fd_flags failed error:0x%x\n",
-			fence_name, *fence_fd);
+			__func__, *fence_fd);
 		mdss_put_sync_fence(sync_fence);
 		sync_fence = NULL;
 		goto end;
